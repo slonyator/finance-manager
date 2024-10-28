147507989f52fad4be6c90c4a09bd853a94182f7
Michael Polonskij <michael.polonskij@gmx.de>
2024-10-28 20:37:30 +0100

    feat: Convert a pdf to image & extract the content

--- text_extraction.py
+++ text_extraction.py
@@ -1,21 +1,35 @@
+import glob
+import os
+from io import BytesIO
+
 import ollama
 from loguru import logger
-
-
+from pdf2image import convert_from_path
+from pyprojroot import here
 
 if __name__ == "__main__":
-    image_path = "/Users/michael/Desktop/Bildschirmfoto 2024-07-20 um 20.51.42.png"
+    pdf_path = str(here("./data/"))
+
+    pdf_files = glob.glob(os.path.join(pdf_path, "*.pdf"))
+
+    pages = convert_from_path(pdf_files[0])
+
+    images_bytes = [
+        (
+            img_byte_arr := BytesIO(),
+            page.save(img_byte_arr, format="PNG"),
+            img_byte_arr.getvalue(),
+        )[2]
+        for page in pages
+    ]
 
     message = {
-        'role': 'user',
-        'content': 'Describe this image:',
-        'images': [image_path]
+        "role": "user",
+        "content": "Describe this image:",
+        "images": [images_bytes[0]],
     }
 
-    response = ollama.chat(
-        model="llava:13b",
-        messages=[message]
-    )
+    response = ollama.chat(model="llava:13b", messages=[message])
 
     logger.info("Response received")
 
