c91ce0ee54a3bbc8e05eb4795ac7abb224fc6cd9
Michael Polonskij <michael.polonskij@gmx.de>
2024-10-30 22:40:51 +0100

    feat: Content extraction converted into a func

--- text_extraction.py
+++ text_extraction.py
@@ -1,6 +1,7 @@
 import glob
 import os
 from io import BytesIO
+from typing import Dict, Any
 from typing import List
 
 import ollama
@@ -34,44 +35,61 @@ class ContentExtractor:
 
         return images_bytes
 
+    def page_content_to_str(self, page: bytes) -> str:
+        """
+        Analyzes a document page and extracts the main content.
+
+        Args:
+            page (bytes): The content of the document page in byte format.
+
+        Returns:
+            str: The extracted content from the document page.
+        """
+        system_message: Dict[str, Any] = {
+            "role": "system",
+            "content": (
+                "You are a document analysis assistant specialized in "
+                "extracting structured information from scanned pages of "
+                "documents. Focus on identifying text, headings, tables, and "
+                "visual elements. When extracting content, preserve any "
+                "structured elements such as bullet points, lists, and tables, "
+                "and provide content in a clean, readable format for ease of "
+                "understanding."
+            ),
+        }
+
+        message: Dict[str, Any] = {
+            "role": "user",
+            "content": (
+                "Analyze this document page and extract the main content. "
+                "Focus on text, headings, tables, and important visual "
+                "elements. Provide a detailed summary that captures the and "
+                "key information while preserving any structured elements like "
+                "lists or bullet points."
+            ),
+            "images": [page],
+        }
+
+        response = ollama.chat(
+            model="llava:13b", messages=[system_message, message]
+        )
+
+        logger.info("Response received")
+
+        extracted_content: str = response["message"]["content"]
+
+        logger.info(f"Content: {extracted_content}")
+
+        return extracted_content
+
 
 if __name__ == "__main__":
     pdf_path = str(here("./data/"))
 
     pdf_files = glob.glob(os.path.join(pdf_path, "*.pdf"))
 
-    content = ContentExtractor().convert_pdf_to_images(pdf_files[0])
+    bytes_content = ContentExtractor().convert_pdf_to_images(pdf_files[0])
 
-    system_message = {
-        "role": "system",
-        "content": (
-            "You are a document analysis assistant specialized in extracting "
-            "structured information from scanned pages of documents. Focus on "
-            "identifying text, headings, tables, and visual elements. When "
-            "extracting content, preserve any structured elements such as "
-            "bullet points, lists, and tables, and provide content in a clean, "
-            "readable format for ease of understanding."
-        ),
-    }
-
-    message = {
-        "role": "user",
-        "content": (
-            "Analyze this document page and extract the main content. "
-            "Focus on text, headings, tables, and important visual elements. "
-            "Provide a detailed summary that captures the and key information "
-            "while preserving any structured elements like lists or "
-            "bullet points."
-        ),
-        "images": [content[0]],
-    }
-
-    response = ollama.chat(
-        model="llava:13b", messages=[system_message, message]
-    )
-
-    logger.info("Response received")
-
-    content = response["message"]["content"]
+    content = ContentExtractor().page_content_to_str(bytes_content[0])
 
     logger.info(f"Content: {content}")
